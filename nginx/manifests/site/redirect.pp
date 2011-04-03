32742f76aaada05d279d5d6be012fbff22f6696d
eric <eric@sietch-tabr.com>
2011-04-03 15:51:42 +0200

    nginx: improved redirect vhost definition: enabled or not, aliases

--- nginx/manifests/site/redirect.pp
+++ nginx/manifests/site/redirect.pp
@@ -1,6 +1,14 @@
 
-define nginx::site::redirect($nginx_servername='', $nginx_redirecturl='', $nginx_port='80', $default_vhost=false, $permanent=true) {
+define nginx::site::redirect($nginx_servername='',
+                             $nginx_serveraliases=[],
+                             $nginx_port='80',
+                             $nginx_redirecturl='',
+                             $default_vhost=false,
+                             $permanent=true,
+                             $enabled=true) {
+
     include nginx::package
+
     $define_servername = $default_vhost ?{
         true    => "$nginx_servername default",
         default => $nginx_servername,
@@ -9,7 +17,23 @@ define nginx::site::redirect($nginx_servername='', $nginx_redirecturl='', $nginx
         false   => "",
         default => "permanent",
     }
-    file { "/etc/nginx/sites-available/${name}":
+    $src_filename = "/etc/nginx/sites-available/${name}"
+    $dst_filename = "/etc/nginx/sites-enabled/${name}"
+
+
+    file { "${src_filename}":
         content => template("nginx/redirect.erb")
+    }
+
+    if $enabled {
+        exec { "ln -s ${src_filename} ${dst_filename}":
+            subscribe => File["${src_filename}"],
+            refreshonly => true,
+        }
+    }
+    else {
+        exec { "rm -f ${dst_filename}":
+            subscribe => file["${src_filename}"],
+        }
     }
 }
