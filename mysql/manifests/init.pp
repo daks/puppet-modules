614ce39fc5ff90f35b4985b175871e696de669db
eric <eric@sietch-tabr.com>
2011-04-03 15:44:56 +0200

    mysql module

---
+++ mysql/manifests/init.pp
@@ -0,0 +1,32 @@
+# modules/mysql/manifest/init.pp
+
+class mysql {
+    include mysql::server
+    include mysql::client
+}
+
+class mysql::client {
+    package { "libmysqlclient16":
+        ensure => installed
+    }
+}
+
+class mysql::server {
+    include mysql::server::package
+    include mysql::server::service
+}
+
+class mysql::server::package {
+    package { "mysql-server":
+        ensure => installed
+    }
+}
+
+class mysql::server::service {
+    service { "mysql":
+        enable => true,
+        ensure => running,
+        require => Class["mysql::server::package"]
+    }
+}
+
