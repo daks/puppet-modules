9a3dcec05f1066bc6e7ab54ce060628bb923f132
eric <eric@sietch-tabr.com>
2011-04-22 13:19:57 +0200

    basic ntp module

---
+++ ntp/manifests/init.pp
@@ -0,0 +1,23 @@
+# modules/ntp/manifests/init.pp
+
+class ntp {
+    include ntp::package
+    include ntp::service
+}
+
+class ntp::package {
+    package { "ntp":
+        ensure => installed,
+    }
+}
+
+class ntp::service {
+    service { "ntp":
+        ensure     => running,
+        enable     => true,
+        hasrestart => true,
+        hasstatus  => true,
+        require    => Class['ntp::package'],
+    }
+}
+    
