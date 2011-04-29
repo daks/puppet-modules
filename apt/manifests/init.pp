490b59b2d1e91af61a7103ee55b8eb2616b74287
eric <eric@sietch-tabr.com>
2011-04-29 17:09:13 +0200

    New apt module, for the moment only sets an HTTP proxy

---
+++ apt/manifests/init.pp
@@ -0,0 +1,29 @@
+# modules/apt/manifests/init.pp
+
+class apt {
+
+    $directories = ["/etc/apt/apt.conf.d", "/etc/apt/sources.list.d"]
+    file { $directories:
+        ensure  => "directory",
+        recurse => true,
+    }
+
+    # apt configuration is managed in the apt.conf.d 
+    # so the apt.conf must not be present
+    file { "/etc/apt/apt.conf":
+        ensure => absent,
+    }
+}
+
+class apt::package {
+    package {"debconf-utils":
+        ensure => installed,
+    }
+}
+
+class apt::proxy {
+    file { "/etc/apt/apt.conf.d/10proxy":
+        ensure  => present,
+        content => template("apt/proxy.erb")
+    }
+}
