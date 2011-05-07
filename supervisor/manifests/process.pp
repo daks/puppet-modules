25b494496a38f1fe39dbf3b97e722b224eb59f2d
eric <eric@sietch-tabr.com>
2011-05-07 23:18:16 +0200

    supervisor module for managing package, service and for creation of process config file

---
+++ supervisor/manifests/process.pp
@@ -0,0 +1,18 @@
+# modules/supervisor/manifests/conf.pp
+
+define supervisor::process($command,
+                           $autostart='true',
+                           $autorestart='true',
+                           $startsecs='1',
+                           $user,
+                           $redirect_stderr='false',
+                           $stdout_logfile='AUTO',
+                           $stderr_logfile='AUTO',
+                           $directory='') {
+
+    $conf_dir = "/etc/supervisor/conf.d"
+    $conf_file = "$conf_dir/$name.conf"
+    file { $conf_file:
+        content => template("supervisor/process.erb"),
+    }
+}
