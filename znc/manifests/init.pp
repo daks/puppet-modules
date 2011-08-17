60abef2fa53a03c1e8286ca0b88540b7021876a7
Eric Veiras Galisson <eric@sietch-tabr.com>
2011-08-17 22:26:10 +0200

    Added znc module to install package and create a supervisor process to run it

---
+++ znc/manifests/init.pp
@@ -0,0 +1,40 @@
+# modules/znc/manifests/init.pp
+
+class znc {
+    include znc::package
+    include znc::user
+    include znc::service
+}
+
+class znc::package {
+    package { ["znc", "znc-extra"]:
+        ensure => installed,
+    }
+}
+
+class znc::user {
+    group { "znc":
+        ensure => present,
+    }
+    user { "znc":
+        ensure  => present,
+        comment => "For znc service",
+        shell   => "/bin/false",
+        home    => "/home/znc",
+        gid     => "znc",
+        require => Group["znc"],
+    }
+}
+
+class znc::service {
+    include znc::service::supervisor
+}
+
+class znc::service::supervisor {
+    include supervisor
+    supervisor::process { "znc":
+        command => "/usr/bin/znc -f -d /home/znc/.znc/",
+        user => "znc",
+    }
+}
+
