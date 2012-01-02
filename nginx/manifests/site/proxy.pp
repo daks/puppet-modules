c88fa630b3e355df63afa2a5ace81269f3a7506d
Eric Veiras Galisson <eric@sietch-tabr.com>
2012-01-02 15:31:53 +0100

    nginx: fixing enabling/disabling site

--- nginx/manifests/site/proxy.pp
+++ nginx/manifests/site/proxy.pp
@@ -12,8 +12,8 @@ define nginx::site::proxy($server_name='',
     include nginx::package
 
     $define_port = $default_vhost ?{
-        true    => "$port default",
-        default => $port,
+        true    => "${port} default",
+        default => "${port}",
     }
     $src_filename = "/etc/nginx/sites-available/${name}"
     $dst_filename = "/etc/nginx/sites-enabled/${name}"
@@ -27,8 +27,8 @@ define nginx::site::proxy($server_name='',
         default => "absent",
     }
 
-    file { $dst_filename:
-        ensure => $ensure,
-        target => $src_filename,
+    file { "${dst_filename}":
+        ensure => "${ensure},
+        target => "${src_filename}",
     }
 }
--- nginx/manifests/site/redirect.pp
+++ nginx/manifests/site/redirect.pp
@@ -10,8 +10,8 @@ define nginx::site::redirect($server_name='',
     include nginx::package
 
     $define_port = $default_vhost ?{
-        true    => "$port default",
-        default => $port,
+        true    => "${port} default",
+        default => "${port}",
     }
     $define_permanent = $permanent ?{
         false   => "",
@@ -20,20 +20,17 @@ define nginx::site::redirect($server_name='',
     $src_filename = "/etc/nginx/sites-available/${name}"
     $dst_filename = "/etc/nginx/sites-enabled/${name}"
 
-
     file { "${src_filename}":
         content => template("nginx/redirect.erb")
     }
 
-    if $enabled {
-        exec { "ln -s ${src_filename} ${dst_filename}":
-            subscribe => File["${src_filename}"],
-            refreshonly => true,
-        }
+    $ensure = $enabled ? {
+        true     => "link",
+        default  => "absent",
     }
-    else {
-        exec { "rm -f ${dst_filename}":
-            subscribe => file["${src_filename}"],
-        }
+
+    file { "${dst_filename}":
+        ensure => "${ensure}",
+        target => "${src_filename}",
     }
 }
