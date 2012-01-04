4f3989a29d6ab0f2ab04ede5f46d0bcb23c51655
Eric Veiras Galisson <eric@sietch-tabr.com>
2012-01-04 14:51:30 +0100

    apache: trying to follow best practices...

--- apache/manifests/init.pp
+++ apache/manifests/init.pp
@@ -10,28 +10,19 @@
 #
 # Sample Usage:
 #
-# [Remember: No empty lines between comments and class definition]
 class apache {
     require apache::params
-    include apache::package
-    include apache::service
-}
 
-class apache::package {
-    require apache::params
     package { "apache":
         name    => "${apache::params::package}",
         ensure  => present,
     }
-}
 
-class apache::service {
-    require apache::params
     service { "apache":
         name    => "${apache::params::service}",
         ensure  => running,
         enable  => true,
-        require => Class["apache::package"],
+        require => Package["apache"],
     }
 }
 
