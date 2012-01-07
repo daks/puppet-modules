6b098cb3f61619db97446650731869475c8fb8e6
Eric Veiras Galisson <eric@sietch-tabr.com>
2012-01-07 20:24:40 +0100

    apt: added a way to disable source

--- apt/manifests/source.pp
+++ apt/manifests/source.pp
@@ -1,7 +1,8 @@
 
-define apt::source($uri, $dist=$lsbdistcodename, $components='main', $src=false) {
+define apt::source($ensure=present, $uri, $dist=$lsbdistcodename, $components='main', $src=false) {
     
     file { "/etc/apt/sources.list.d/${name}.list":
+        ensure  => $ensure,
         content => template("apt/sources.list.erb"),
         notify  => Class["apt::update"],
     }
