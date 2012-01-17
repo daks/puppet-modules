c7e0664d62c7e1d3ec77d4424d2f2b4258b2594a
Eric Veiras Galisson <eric@sietch-tabr.com>
2012-01-17 16:20:37 +0100

    initial basic puppet module, implementing agent service and configuration

---
+++ puppet/manifests/init.pp
@@ -0,0 +1,40 @@
+
+class puppet {
+    include puppet::agent
+}
+
+class puppet::agent {
+    include puppet::package
+    include puppet::service
+}
+
+class puppet::package {
+    package { "puppet":
+        ensure => installed,
+    }
+
+    file { "/etc/puppet":
+        ensure => "directory",
+    }
+}
+
+class puppet::service {
+    file { "/etc/default/puppet":
+        ensure => "present",
+        source => "puppet:///modules/${module_name}/default_puppet",
+    }
+
+    file { "/etc/puppet/puppet.conf":
+        ensure => "present",
+        content => template("modules/${module_name}/puppet.conf.erb"),
+    }
+
+    service { "puppet":
+        ensure => running,
+        enable => true,
+        hasrestart => true,
+        hasstatus => true,
+        require => [ Class["puppet::package"], File["/etc/default/puppet"], File["/etc/puppet/puppet.conf"], ],
+    }
+}
+
