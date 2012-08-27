f0aaa8d96ef48919c796dab02888f4d6f1b2342d
Eric Veiras Galisson <eric@sietch-tabr.com>
2012-08-27 15:04:26 +0200

    snmp: send /etc/default/snmpd (Debian) to configurer service logging

--- snmp/manifests/params.pp
+++ snmp/manifests/params.pp
@@ -2,6 +2,7 @@ class snmp::params {
 
     $srv_pkg = "snmpd"
     $directory = "/etc/snmp"
+    $default_conffile = "/etc/default/snmpd"
     $srv_conffile = "/etc/snmp/snmpd.conf"
     $srv_name = "snmpd"
 }
--- snmp/manifests/server.pp
+++ snmp/manifests/server.pp
@@ -9,6 +9,18 @@ class snmp::server {
     file { "${snmp::params::directory}":
         ensure  => present,
     }
+    file { "${snmp::params::default_conffile}":
+        ensure  => file,
+        source  => [
+                    "puppet:///files/${fqdn}/etc/default/snmpd",
+                    "puppet:///files/${hostgroup}/etc/default/snmpd",
+                    "puppet:///files/${domain}/etc/default/snmpd",
+                    "puppet:///files/global/etc/default/snmpd",
+                    "puppet:///modules/${module_name}/default_snmpd",
+                   ],
+        notify  => Service["${snmp::params::srv_name}"],
+    }
+ 
     file { "${snmp::params::srv_conffile}":
         ensure  => file,
         source  => [
